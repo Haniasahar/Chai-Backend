8f215794ab758d32f560f621e5d05f4e96e67a66
Haniya Sahar <umerzaheer9999@gmail.com>
2025-07-10 14:48:39 +0500

    connected to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("haha")
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+console.log("haha");
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+// (async()=>{
+//     try {
+//       await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// })()
+
+dotenv.config({
+    path: "./. env"
+});
+
+connectDB();
