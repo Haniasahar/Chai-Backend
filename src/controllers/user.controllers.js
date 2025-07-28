8e000e4d0a1c29117ef587c9c80d933c7b4e72df
Haniya Sahar <umerzaheer9999@gmail.com>
2025-07-28 14:54:49 +0500

    added models

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,3 +6,29 @@ res.status(200).json({
 })
 }
 )
+
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     userName: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         lowercase: true,
+//     },
+
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         lowercase: true,
+//     },
+
+//     password: {
+//         type: String,
+//         required: true,
+//     }
+
+// }, { timestamps: true });
+
+// export const User = mongoose.model("User", userSchema);
