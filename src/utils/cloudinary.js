34dd4fd987dbf7bf85dc6e22b12906ddac31361d
Haniya Sahar <umerzaheer9999@gmail.com>
2025-07-28 14:52:38 +0500

    added files

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv"
+
+dotenv.config({
+  path: "./.env",
+});
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadResult=async (localFilePath)=>{
+try{
+if(!localFilePath) {return console.log("File Not Found");}
+cloudinary.uploader.upload(localFilePath, {
+    resource_type:"auto"
+})
+console.log("File Successfully Uploaded", response.url)
+return response;
+}
+catch(err){
+fs.unlinkSync(localFilePath)
+return null;
+}
+}
+
+export {uploadResult};
