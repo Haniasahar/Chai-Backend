430b084deeff4faa5f3f2d1e17810212e572446b
Haniya Sahar <umerzaheer9999@gmail.com>
2025-07-31 03:01:42 +0500

    modified controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,18 +1,37 @@
+import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { userName, fullName, email, password } = req.body;
+  console.log(email);
 
-const registerUser=asyncHandler(async (req,res)=>{
-const {userName,fullName,email,password}=req.body;
-console.log(email);
+  if (
+    [fullName, userName, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required ");
+  }
 
-if(fullName===""){
-throw new ApiError(400)
+  const existedUser = User.findOne({
+    $or: [{ email }, { userName }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  console.log(req.files?.avatar[0]?.path);
+
+  const avatarLocalPath=req.files?.avatar[0]?.path;
+  const coverImageLocalPath=req.files?.coverImage[0]?.path;
+
+if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
 }
-}
-)
 
-export {registerUser}
+});
+
+export { registerUser };
 
 // import mongoose from "mongoose";
 
